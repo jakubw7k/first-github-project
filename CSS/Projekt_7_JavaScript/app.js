1053d64c015ea5ea90108cbbf03f4d6b6647fd52
Jakub Wachowiak <104766988+notfirsttimecoding@users.noreply.github.com>
2022-08-08 04:44:06 +0200

    abc

---
+++ CSS/Projekt_7_JavaScript/app.js
@@ -0,0 +1,9 @@
+function reverseString(str){
+    let reversedString='';
+    for (let i = 0; i < str.length; ++i) {
+        reversedString =  str[i] + reversedString;  
+    }
+    return reversedString; 
+}
+
+console.log(reverseString("Jakub"));
