4b9e8fc280d727de59e0c26ffb7ffe5c52a33e16
Jakub Wachowiak <104766988+notfirsttimecoding@users.noreply.github.com>
2022-08-14 06:34:50 +0200

    Added Java Script and ready for responsiveness

--- CSS/Projekt_8_My_Portfolio_V2/index.js
+++ CSS/Projekt_8_My_Portfolio_V2/index.js
@@ -1,6 +1,29 @@
-//template_2lx65l7
-//service_w3m782l
-//Iv8AN27VVznZnSVf7
+let isModalOpen = false;
+let contrastToggle=false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+  }
+
+function toggleContrast() {
+    contrastToggle =!contrastToggle;
+    if(contrastToggle) {
+    document.body.classList +=" dark-theme";
+}
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -24,4 +47,16 @@ function contact(event) {
           "The email service is temporarily unavailable. Please contact me directly on email@email.com"
         );
       });
+  }
+
+
+
+  function toggleModal(){
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+
+    isModalOpen = true;
+    document.body.classList+= " modal--open"
   }
